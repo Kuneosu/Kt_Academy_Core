91fa2f2a7fd4bd550277b9d9b21784171330c881
Kuneosu <kks92155784@gmail.com>
2024-09-19 10:27:23 +0900

    스플래시 화면 구현

--- app/src/main/java/com/kuneosu/kotlinacademycore/MainActivity.kt
+++ app/src/main/java/com/kuneosu/kotlinacademycore/MainActivity.kt
@@ -5,21 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kuneosu.kotlinacademycore.ui.theme.KotlinAcademyCoreTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             KotlinAcademyCoreTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = "Main Activity")
+                }
             }
         }
     }
